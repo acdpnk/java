6b365bd4b85f6e775d4f685ac3f20f082a39aeb3
chrifpa <chrifpa@gmail.com>
2014-04-16 13:59:30 +0200

    horribly inefficient checking algorithm: check

--- ue1/src/BasicQueenSolver.java
+++ ue1/src/BasicQueenSolver.java
@@ -1,39 +1,48 @@
-public  class BasicQueenSolver{
+public /*abstract*/ class BasicQueenSolver{
     //public abstract void solve();
     public boolean checkChessboard(Chessboard board){
         int size = board.getSize();
-        int[] rows = new int[size];
-        for(int i=0; i<size; i++){
-            int pos = -1; //can't use null, -1 is not a valid value so we use that.
-            for(int j=0; j<size; j++){
-                if(board.get(i,j) && pos!=-1){
-                    return false;
-                }
-                else if (board.get(i,j)){pos=j;}
-            }
-            rows[i]=pos;
-            System.out.println(rows);
-        }
-
-        for (int k=0; k<size; k++){
-            for (int l=0; l<size; l++){
-                if (rows[k]<0 && rows[l]<0 || k==l){
+        for(int i=0; i<size; i++){ //iterate over rows
+            for(int j=0; j<size; j++){ //iterate over columns
+                // we want to check for pieces threatening i,j
+                // which only makes sense if there's a piece here
+                if (! board.get(i,j)){
                     continue;
                 }
-                else if (rows[k]==rows[l]){
-                    System.out.println("meep");
-                    return false;
-                }
-                else if (k+l<size && rows[l]==k+l){
-                    return false;
-                }
-                else if (k-l>=0 && rows[l]==k-l){
-                    return false;
+
+                //for(int k=i+1; k<size; k++){
+                    //if(board.get(i,k)){
+                        //// there's another piece on this row =>
+                        //System.out.println("row");
+                        //return false;
+                    //}
+                //}
+
+                for(int l=1; i+l<size || j+l<size; l++){
+                   if(i+l<size && board.get(i+l,j)){
+                       // there's another piece on this column =>
+                        System.out.println("col");
+                        return false;
+                    }
+                   if(j+l<size && board.get(i,j+l)){
+                         // there's another piece on this row =>
+                        System.out.println("row");
+                        return false;
+                    }
+                   if(i-l>0 && j+l<size && board.get(i-l,j+l)){
+                        // there's another piece on the lower left diagonale =>
+                        System.out.println("loleft");
+                        return false;
+                    }
+                    if(i+l<size &&j+l<size && board.get(i+l,j+l)){
+                        // there's another piece on the lower right diagonale =>
+                        System.out.println("loright");
+                        return false;
+                    }
                 }
             }
         }
-
+        // all clear =>
         return true;
     }
-
 }
--- ue1/src/Test.java
+++ ue1/src/Test.java
@@ -2,7 +2,7 @@ public class Test{
     public static void main(String[] args){
         Chessboard board = new Chessboard(8);
         board.set(true,4,4);
-        board.set(true, 5,6);
+        board.set(true, 5,5);
 
         System.out.println(board);
 
