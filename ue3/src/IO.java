0b8868e198e65102749b17ed095cdf998387f13f
chrifpa <chrifpa@gmail.com>
2014-04-29 18:33:39 +0200

    implemented the saveResult method of IO

--- ue3/src/IO.java
+++ ue3/src/IO.java
@@ -3,7 +3,10 @@ import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
-
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.util.Map;
 public class IO {
     public static List<Question> readQuestions(String filename){
         ArrayList<Question> questions = new ArrayList<Question>();
@@ -40,5 +43,43 @@ public class IO {
             }
         }
         return questions;
+    }
+
+
+    public static void saveResult(IStatisticController controller, String playername){
+        String filename = playername + "_" + System.currentTimeMillis();
+        BufferedWriter bw = null;
+
+        try {
+            bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"));
+
+            bw.write("Player Name: " + playername);
+            bw.newLine();
+            bw.write("Richtige Antworten: " + controller.getRightAnswers());
+            bw.newLine();
+            bw.write("Falsche Antworten: " + controller.getWrongAnswers());
+
+            bw.newLine();
+            bw.newLine();
+
+            Map<Question, String> answers = controller.getAnswers();
+
+            for(Map.Entry<Question, String> entry : answers.entrySet()){
+                bw.write(entry.getKey().getQuestion() + ", " +
+                         entry.getValue() + ", " +
+                         entry.getKey().checkAnswer(entry.getValue()));
+                bw.newLine();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
