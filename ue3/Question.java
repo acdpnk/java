d6df2d064c6412c128d6f99bbc1cbc6a6b47e304
chrifpa <chrifpa@gmail.com>
2014-04-29 10:22:53 +0200

    implemented question object

---
+++ ue3/Question.java
@@ -0,0 +1,27 @@
+public class Question {
+    private String question;
+    private String[] answers;
+    private int solution;
+
+    public Question(String question, String[] answers, int solution){
+        this.question = question;
+        this.answers = answers;
+        this.solution = solution;
+    }
+
+    public String getQuestion(){
+        return this.question;
+    }
+
+    public String[] getAnswers(){
+        return this.answers;
+    }
+
+    public boolean checkAnswer(String answer){
+        String solution = this.answers[this.solution];
+        if(solution.equalsIgnoreCase(answer)){
+            return true;
+        }
+        return false;
+    }
+}
