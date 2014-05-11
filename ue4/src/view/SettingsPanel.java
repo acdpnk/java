22efe40e2ef1a0ad852ce53a9072f5343a46dc3b
chrifpa <chrifpa@gmail.com>
2014-05-11 10:57:32 +0200

    added JFileChooser

--- ue4/src/view/SettingsPanel.java
+++ ue4/src/view/SettingsPanel.java
@@ -9,17 +9,23 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JFileChooser;
-
+import java.io.File;
 
 public class SettingsPanel extends JPanel{
 
-    static final long serialVersionUID = 23; //this is here to stop javac from complaining.
+    static final long serialVersionUID = 23; // this is here to stop javac from complaining.
     public static final int STD_NUMBER_OF_ROUNDS = 10;
 
+    // gui elements
     private JLabel playerLabel, roundsLabel, fileLabel;
     private JPanel playerPanel, roundsPanel, filePanel, goPanel;
     private JTextField playerField, roundsField;
-    private JButton chooseFileButton, goButton;
+    private JButton chooseFileButton, goButton, cancelButton;
+
+    // actual data
+    private String questionFile = null;
+    private String player = "";
+    private int rounds = STD_NUMBER_OF_ROUNDS;
 
     public SettingsPanel(int stdNumberOfRounds){
         this.roundsLabel = new JLabel("Number of Rounds per Game:");
@@ -32,6 +38,7 @@ public class SettingsPanel extends JPanel{
         this.chooseFileButton = new JButton("Choose a Question File");
 
         this.goButton = new JButton("Go!");
+        this.cancelButton = new JButton("Cancel");
 
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
@@ -57,12 +64,33 @@ public class SettingsPanel extends JPanel{
 
         this.goPanel = new JPanel();
         this.goPanel.setLayout(new BoxLayout(goPanel, BoxLayout.LINE_AXIS));
+        this.goPanel.add(cancelButton);
         this.goPanel.add(goButton);
         this.add(goPanel);
 
         this.playerField.setColumns(12);
         this.roundsField.setColumns(3);
         this.setVisible(true);
+
+        this.goButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+
+            }
+        });
+
+        this.chooseFileButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                JFileChooser chooser = new JFileChooser();
+                chooser.showOpenDialog(filePanel);
+                File file = chooser.getSelectedFile();
+                if(file == null){
+                    return;
+                }
+                fileLabel.setText(file.getName());
+                questionFile = file.getPath();
+            }
+        });
+
 
     }
     public SettingsPanel(){
