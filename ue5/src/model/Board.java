8fe65505aa3f4f1a2b12a9a042e25553875b2626
chrifpa <chrifpa@gmail.com>
2014-05-22 18:49:22 +0200

    board now works with modulo arithmetic and will therefore behave like a circle.

--- ue5/src/model/Board.java
+++ ue5/src/model/Board.java
@@ -4,23 +4,24 @@ import java.util.Observable;
 
 public class Board extends Observable
 {
+    public static final int NUM_PITS = 14;
     private int[] pits;
 
     public Board()
     {
-        pits = new int[14];
+        pits = new int[NUM_PITS];
     }
 
     public int getSeeds(int pit)
     {
-        return pits[pit];
+        return pits[pit % NUM_PITS];
     }
 
     public void setSeeds(int pit, int seeds)
     {
-        pits[pit] = seeds;
+        pits[pit % NUM_PITS] = seeds;
         setChanged();
-        notifyObservers(pit);
+        notifyObservers(pit % NUM_PITS);
     }
 
 
